7e638927ed5862dfef6ea5a235cb3704fbfacb1a
Alexander Andreev <andr33v-ae@yandex.ru>
2020-09-11 17:16:07 +0300

    Initial Commit

---
+++ Donations.Mobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Donations.Mobile
+//
+//  Created by Александр Андреев on 11.09.2020.
+//  Copyright © 2020 Александр Андреев. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
